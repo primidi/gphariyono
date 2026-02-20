9319acf17fc0f35dd7ed5bd1dc452f31ffeb1011
Gharis Primada Hariyono <hariyonogharis@gmail.com>
2026-02-20 11:03:17 +0700

    feat: netlify for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,13 @@ import react from '@astrojs/react'
 import mdx from '@astrojs/mdx'
 import tailwindcss from '@tailwindcss/vite'
 import sitemap from '@astrojs/sitemap'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://gphariyono.com',
   integrations: [react(), mdx(), sitemap()],
-
+  adapter: netlify(),
   vite: {
     plugins: [tailwindcss()]
   }
